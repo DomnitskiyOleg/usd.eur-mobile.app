8ff5465b00b69ad83e76e27151318e2805b2a03f
Oleg Domnitskiy <domnitskiy.oleg@mail.ru>
2023-08-16 18:31:21 +0600

    refactoring

--- src/getUrlsForFetch.js
+++ src/getUrlsForFetch.js
@@ -1,23 +1,20 @@
 const usdCode = 'R01235';
 const eurCode = 'R01239';
 
-const getRequests = (requestUrl, day, month, year) => {
-  return {
-    usdRequest: `${requestUrl}?date_req1=${day}/${month}/${
-      year - 1
-    }&date_req2=${day}/${month}/${year}&VAL_NM_RQ=${usdCode}`,
-    eurRequest: `${requestUrl}?date_req1=${day}/${month}/${
-      year - 1
-    }&date_req2=${day}/${month}/${year}&VAL_NM_RQ=${eurCode}`,
-  };
-};
+const requestUrl = 'https://cbr.ru/scripts/XML_dynamic.asp';
+const requestUrlForActualRates = 'https://www.cbr.ru/scripts/XML_daily.asp';
+
+const getRequests = (requestUrl, day, month, year, code) =>
+  `${requestUrl}?date_req1=${day}/${month}/${
+    year - 1
+  }&date_req2=${day}/${month}/${year}&VAL_NM_RQ=${code}`;
 
 export default function getUsdEurUrls() {
   const currentDate = new Date(Date.now());
   const [year, month, day] = currentDate.toISOString().slice(0, 10).split('-');
-  const requestUrl = 'https://cbr.ru/scripts/XML_dynamic.asp';
-  const requestUrlForActualRates = 'https://www.cbr.ru/scripts/XML_daily.asp';
-  const { usdRequest, eurRequest } = getRequests(requestUrl, day, month, year);
+
+  const usdRequest = getRequests(requestUrl, day, month, year, usdCode);
+  const eurRequest = getRequests(requestUrl, day, month, year, eurCode);
 
   return [usdRequest, eurRequest, requestUrlForActualRates];
 }
