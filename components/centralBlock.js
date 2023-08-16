b98bae4ad2a92a1a4336e7150e366bd8ca67c06c
Oleg Domnitskiy <domnitskiy.oleg@mail.ru>
2023-08-16 17:54:18 +0600

    delete similar components

--- components/centralBlock.js
+++ components/centralBlock.js
@@ -3,65 +3,23 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { Text, View, Image, Dimensions } from 'react-native';
 
 export default (props) => (
-  <View style={styles.central}>
+  <View style={styles.central_block}>
     <LinearGradient colors={['#fb8c00', '#ffa726']} style={styles.linear}>
       <Text style={styles.text}>TODAY'S USD RATE</Text>
       <Image
         source={require('../pictures/usa-flag.png')}
         style={styles.logos}
       />
-      <View style={styles.valueContainer1}>
-        <Text
-          style={{
-            fontSize: Dimensions.get('window').width * 0.062,
-            margin: 2,
-            opacity: 0.9,
-            color: 'black',
-            fontWeight: '700',
-            textShadowRadius: 2,
-            textShadowColor: 'black',
-          }}
-        >
-          {props.usdActualValue}
-        </Text>
-        <Text
-          style={{
-            fontSize: 11,
-            margin: 0,
-            textShadowRadius: 2.5,
-            textShadowColor: 'black',
-          }}
-        >
-          {props.currentDate}
-        </Text>
+      <View style={styles.valueContainer}>
+        <Text style={styles.central_text}>{props.usdActualValue}</Text>
+        <Text style={styles.central_date}>{props.currentDate}</Text>
       </View>
     </LinearGradient>
     <LinearGradient colors={['#fb8c00', '#ffa726']} style={styles.linear}>
       <Text style={styles.text}>TODAY'S EUR RATE</Text>
-      <View style={styles.valueContainer2}>
-        <Text
-          style={{
-            fontSize: Dimensions.get('window').width * 0.062,
-            margin: 2,
-            opacity: 0.9,
-            color: 'black',
-            fontWeight: '700',
-            textShadowRadius: 2,
-            textShadowColor: 'black',
-          }}
-        >
-          {props.eurActualValue}
-        </Text>
-        <Text
-          style={{
-            fontSize: 11,
-            margin: 0,
-            textShadowRadius: 2.5,
-            textShadowColor: 'black',
-          }}
-        >
-          {props.currentDate}
-        </Text>
+      <View style={styles.valueContainer}>
+        <Text style={styles.central_text}>{props.eurActualValue}</Text>
+        <Text style={styles.central_date}>{props.currentDate}</Text>
       </View>
       <Image
         source={require('../pictures/europe-flag.png')}
